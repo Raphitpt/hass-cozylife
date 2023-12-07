6c7924aa4d22e80bf53d5771ed5143b6c94fe4e3
Raphitpt <tiphonetraphi@gmail.com>
2023-12-07 16:11:29 +0100

    Refactor get_pid_list to use async/await

--- custom_components/cozylife/utils.py
+++ custom_components/cozylife/utils.py
@@ -2,20 +2,11 @@ import json
 import time
 import requests
 import logging
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 _LOGGER = logging.getLogger(__name__)
 
-def get_sn() -> str:
-    """
-    message sn
-    :return: str
-    """
-    return str(int(round(time.time() * 1000)))
-
-# cache get_pid_list result for many calls
-_CACHE_PID = []
-
-def get_pid_list(lang='en') -> list:
+async def get_pid_list(hass, lang='en') -> list:
     """
     http://doc.doit/project-12/doc-95/
     :param lang:
@@ -29,7 +20,13 @@ def get_pid_list(lang='en') -> list:
     protocol = 'http'
     url_prefix = protocol + '://' + domain
     try:
-        res = requests.get(url_prefix + '/api/device_product/model', params={'lang': lang}, timeout=3)
+        # Utilisez `await hass.async_add_executor_job()` pour faire la requête de manière asynchrone
+        res = await hass.async_add_executor_job(
+            requests.get,
+            url_prefix + '/api/device_product/model',
+            params={'lang': lang},
+            timeout=3
+        )
         res.raise_for_status()  # Raise an HTTPError for bad responses
     except requests.exceptions.RequestException as e:
         _LOGGER.error(f'Error making API request: {e}')
