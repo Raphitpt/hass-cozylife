4ba8de016c13c85aea776e21e5521d3616177732
Raphitpt <tiphonetraphi@gmail.com>
2023-12-07 14:29:47 +0100

    Add IP validation and discovery step

--- custom_components/cozylife/config_flow.py
+++ custom_components/cozylife/config_flow.py
@@ -1,15 +1,23 @@
-"""Config flow for CozyLife Local Pull integration."""
 import voluptuous as vol
+import ipaddress
 from homeassistant import config_entries
 
 from .const import DOMAIN
 
 DATA_SCHEMA = vol.Schema({
-    vol.Required('ip_address', description="Adresse IP de l'appareil"): str,
+    vol.Required('ip_start', description="IP de début"): str,
+    vol.Required('ip_end', description="IP de fin"): str,
     vol.Required('port', description="Port de l'appareil", default=5555): int,
     # Ajoutez d'autres options de configuration au besoin
 })
 
+def is_valid_ip(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        return False
+    
 class CozyLifeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a CozyLife Local Pull config flow."""
 
@@ -34,5 +42,32 @@ class CozyLifeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def _is_valid_configuration(self, user_input):
         """Validez les options de configuration."""
-        # Implémentez votre logique de validation ici
+        ip_start = user_input['ip_start']
+        ip_end = user_input['ip_end']
+        port = user_input['port']
+
+        # Vérifiez si les adresses IP sont au format correct
+        if not is_valid_ip(ip_start) or not is_valid_ip(ip_end):
+            return False
+
+        # Vérifiez si le port est dans une plage valide (par exemple, entre 1 et 65535)
+        if not 1 <= port <= 65535:
+            return False
+
         return True
+    
+
+
+    async def async_step_discovery(self, user_input=None):
+        """Handle discovery step."""
+        if user_input is not None:
+            # L'utilisateur a choisi un appareil découvert, traitez les données
+            return self.async_create_entry(title='CozyLife Demo', data=user_input)
+
+        # Affichez une liste d'appareils découverts
+        return self.async_show_form(
+            step_id='discovery',
+            data_schema=vol.Schema({
+                vol.Required('discovered_device', description="Appareil découvert"): str,
+            }),
+        )
